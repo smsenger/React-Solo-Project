395656368df2b160591f9d220a5e432a9ca8f3f3
Saesha Senger <saesha.senger@gmail.com>
2020-08-18 10:28:17 -0400

    changes notes text to desccribe box formatting

--- react-project/src/components/Notes.jsx
+++ react-project/src/components/Notes.jsx
@@ -22,22 +22,22 @@ class Notes extends Component {
             <div>
                 <Jumbotron className="jumbo-background">
                     <h1 className="app-title"><i>One_Stop</i> Notecards</h1>
-                    <p className="app-subtitle">Save or export, all from one form!</p>
+                    <p className="app-subtitle">Flexibly format text boxes, save or export text, all in one place!</p>
                     <p>
                         <Link className="app-link" to="/" variant="primary">Home</Link>
                         <Link className="app-link" to="/searchrequest" variant="primary">News</Link>
                         <Link className="app-link" to='/todoform' variant="primary">Todos</Link>
                         <Link className="app-link" to='/email' variant="primary">Email</Link>
                         <br />
-                        <Button className="app-link" onClick={() => {this.props.addCard()}}>Create card</Button>
+                        <Button className="app-link" onClick={() => { this.props.addCard() }}>Create card</Button>
                     </p>
                     <ResponsiveGridLayout name='card' className="layout"
-                        breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-                        cols={{lg: 8, md: 4, sm: 4, xs: 2, xxs: 2}}>
+                        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+                        cols={{ lg: 8, md: 4, sm: 4, xs: 2, xxs: 2 }}>
                         {this.props.cards.map((card, index) => {
                             return (
-                                <div  data-grid={{ i: 'c', x: 4, y: 0, w: 2, h: 1.5 }} key={index} >
-                                    <NoteCard data={card} id={index}/>
+                                <div data-grid={{ i: 'c', x: 4, y: 0, w: 2, h: 1.5 }} key={index} >
+                                    <NoteCard data={card} id={index} />
                                 </div>
                             )
                         })}
@@ -46,7 +46,7 @@ class Notes extends Component {
 
             </div>
 
-            
+
         )
     }
 }
