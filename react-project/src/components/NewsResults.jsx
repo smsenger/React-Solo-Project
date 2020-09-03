e2b108f520a5931fd205e6c70a6ebe78390fc74d
Saesha Senger <saesha.senger@gmail.com>
2020-09-03 13:58:13 -0400

    new api try

--- react-project/src/components/NewsResults.jsx
+++ react-project/src/components/NewsResults.jsx
@@ -1,30 +1,30 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './NewsResults.css';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 import './NewsResults.css'
 
 export default class SearchResults extends Component {
-    constructor(props) {
-        super(props)
-    }
-
 
     render() {
         return (
             <>
                 <Container>
-                    <Row style={{ margin: 'auto' }}>
+                    <Row className="rowMargins">
                         <Col md="auto" id="row">
-                            <Card className="card" style={{ border: '2px solid #1dc4fd', marginTop: '15px' }}>
-                                <Card.Img variant="top" className="news-image" src={this.props.article.urlToImage} />
+                            <Card className="card" style={{ border: '2px solid #1dc4fd', marginTop: '25px' }}>
+                                <Card.Img variant="top" className="news-image" src={this.props.article.image} />
                                 <Card.Body>
                                     <Card.Title className="card-title" style={{ border: 'none' }}>{this.props.article.title}</Card.Title>
                                     <Card.Title className="card-name" style={{ border: 'none' }}>{this.props.article.source.name}</Card.Title>
                                     <Card.Text className="card-body" style={{ border: 'none' }}>{this.props.article.description}</Card.Text>
+                                    <Button className="card-body" style={{ border: 'none' }} type="submit" href={this.props.article.url}>Click here for full article</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
